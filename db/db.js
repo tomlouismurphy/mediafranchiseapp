bcf612ced2608861a79a448687b1070ce1cedbfa
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-10-23 10:30:13 -0500

    first branch commit

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost/franchiseapp';
+const connectionString = process.env.DB_HOST;
 
 mongoose.connect(connectionString);
 
--- models/movies.js
+++ models/movies.js
@@ -1,32 +1,46 @@
-const movies = [
-    {
-        name: 'The Lion King',
-        releaseDate: 1994,
-        director: ['Roger Allers', 'Rob Minkoff']
-    },
-    {
-        name: 'Pinocchio',
-        releaseDate: 1940,
-        director: ['Norman Ferguson', 'T. Hee', 
-                'Wilfred Jackson', 'Jack Kinney',
-                'Hamilton Luske', 'Bill Roberts',
-                'Ben Sharpsteen']
-    },
-    {
-        name: 'Mulan',
-        releaseDate: 1998,
-        director: ['Tony Bancroft', 'Barry Cook']
-    },
-    {
-        name: 'Robin Hood',
-        releaseDate: 1973,
-        director: ['Wolfgang Reitherman']
-    },
-    {
-        name: 'The Rescuers',
-        releaseDate: 1977,
-        director: ['John Lounsbery', 'Wolfgang Reitherman']
-    }
-];
+const mongoose = require('mongoose');
+
+const MovieSchema = new mongoose.Schema({
+    name: String,
+    releaseDate: Number,
+    director: [String],
+    characters: [String]
+})
+
+// const movies = [
+//     {
+//         name: 'The Lion King',
+//         releaseDate: 1994,
+//         director: ['Roger Allers', 'Rob Minkoff'],
+//         characters: ['Simba']
+//     },
+//     {
+//         name: 'Pinocchio',
+//         releaseDate: 1940,
+//         director: ['Norman Ferguson', 'T. Hee', 
+//                 'Wilfred Jackson', 'Jack Kinney',
+//                 'Hamilton Luske', 'Bill Roberts',
+//                 'Ben Sharpsteen'],
+//         characters: ['Pinocchio']
+//     },
+//     {
+//         name: 'Mulan',
+//         releaseDate: 1998,
+//         director: ['Tony Bancroft', 'Barry Cook'],
+//         characters: ['Mushu']
+//     },
+//     {
+//         name: 'Robin Hood',
+//         releaseDate: 1973,
+//         director: ['Wolfgang Reitherman'],
+//         characters: ['Sir Hiss']
+//     },
+//     {
+//         name: 'The Rescuers',
+//         releaseDate: 1977,
+//         director: ['John Lounsbery', 'Wolfgang Reitherman'],
+//         characters: ['Bernard']
+//     }
+// ];
 
 module.exports = movies;
