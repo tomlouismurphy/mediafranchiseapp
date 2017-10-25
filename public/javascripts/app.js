c36eb4d5d06c566c086ec3139272efc184b8a152
Sam Sheehan <ssheehan1210@yahoo.com>
2017-10-25 11:33:27 -0500

    Created event listener that allows users to create new comments, though it currently cannot save the comments as it is now

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,7 @@
 console.log('the public js file is running');
 
+let commentCounter = 1;
+
 // API1: Walt Disney Pictures - 440 movies - 22 pages (https://www.themoviedb.org/company/2)
 // API2: Walt Disney Productions - 580 movies - 29 pages (https://www.themoviedb.org/company/3166)
 // API3: Disneynature - 8 movies - 1 page (https://www.themoviedb.org/company/4436)
@@ -118,7 +120,6 @@ $.ajax({
 		for (a = 0; a < $movieGenres.length; a++) {
 			let $movieGenresLi = $('<li>');
 			$movieGenresLi.attr('class', 'text-left');
-			// $movieGenresLi.css('text-align', 'left');
 			if ($movieGenres[a] === 28) {
 				$movieGenresUl.append($movieGenresLi);
 				$movieGenresLi.text("Action");
@@ -308,6 +309,25 @@ $('#third-textarea-layout').on('shown.bs.collapse', () => {
 
 $('#third-textarea-layout').on('hide.bs.collapse', () => {
 	$('#third-textarea-layout').css('display', 'none');
+})
+
+
+
+$('#post-comment-button').on('click', () => {
+	const $newComment = $('#new-user-comment')[0].value;
+	const $newCommentEntry = $('<tr>');
+	$('#user-comments-list').append($newCommentEntry);
+	const $newCommentNumber = $('<th>');
+	$newCommentNumber.attr('scope', 'row');
+	$newCommentEntry.append($newCommentNumber);
+	commentCounter += 1;
+	$newCommentNumber.text(`${commentCounter}`);
+	const $newCommentUsername = $('<td>');
+	$newCommentEntry.append($newCommentUsername);
+	$newCommentUsername.text('username');
+	const $newCommentContent = $('<td>');
+	$newCommentEntry.append($newCommentContent);
+	$newCommentContent.text($newComment);
 })
 
 console.log('public js window is now closing');
