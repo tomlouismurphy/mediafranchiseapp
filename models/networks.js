c1b3a34285c4823d8d115209953cc8b0e72bccf3
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-10-26 09:49:45 -0500

    removed surplus js model files

--- models/networks.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const findNetworkSchema = new mongoose.Schema({
-	id: Number,
-	name: String
-})
-
-module.exports = mongoose.model('Network', findNetworkSchema);
